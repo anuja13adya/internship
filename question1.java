a3e0f6c0d2cc0f1e207672bc5093a5c4377f9a1f
anuja13adya <49363072+anuja13adya@users.noreply.github.com>
2021-03-31 09:42:11 +0530

    Add files via upload

---
+++ question1.java
@@ -0,0 +1,12 @@
+public class question1 {
+    public static void main(String[] args)
+    {
+        int a=10;
+        double b=10.0;
+        char c='A';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
