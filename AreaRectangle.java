c55145f3d8c134cb1c64f7a1880d9c4e5a240a54
anuja13adya <49363072+anuja13adya@users.noreply.github.com>
2021-03-31 09:47:17 +0530

    Add files via upload

---
+++ AreaRectangle.java
@@ -0,0 +1,9 @@
+public class AreaRectangle {
+    public static void main(String[] args)
+    {
+        int l=3;
+        int b=2;
+
+        System.out.println("Area of Rectangle is "+l*b);
+    }
+}
