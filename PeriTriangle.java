6c82408f2d309557e856447b81bfc3b3d9a251ca
anuja13adya <49363072+anuja13adya@users.noreply.github.com>
2021-03-31 13:59:25 +0530

    Add files via upload

---
+++ PeriTriangle.java
@@ -0,0 +1,8 @@
+public class PeriTriangle {
+    public static void main(String[] args) {
+        int x=2;
+        int y=3;
+        int z=5;
+        System.out.println("Perimeter of triangle is "+x*y*z);
+    }
+}
