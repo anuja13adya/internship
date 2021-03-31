9ad0e671d712ddb4514e523a157107060065ce05
anuja13adya <49363072+anuja13adya@users.noreply.github.com>
2021-03-31 13:55:45 +0530

    Add files via upload

---
+++ AreaPeriRectangle.java
@@ -0,0 +1,10 @@
+public class AreaPeriRectangle {
+    public static void main(String[] args)
+    {
+        int l=5;
+        int b=7;
+
+        System.out.println("Area of Rectangle is "+l*b);
+        System.out.println("Perimeter of Rectangle is "+(l+b));
+    }
+}
