75490e7f8278c94770cc930b85646b9e7d4be986
anuja13adya <49363072+anuja13adya@users.noreply.github.com>
2021-03-31 11:39:24 +0530

    Add files via upload

---
+++ EquivalentCharacter.java
@@ -0,0 +1,8 @@
+public class EquivalentCharacter {
+    public static void main(String[] args) {
+        char x='d';
+        int z=x+3;
+        x=(char)z;
+        System.out.println("Equivalent Character is "+x);
+    }
+}
